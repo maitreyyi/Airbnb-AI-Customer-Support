51cd38e7bec825d6e008fe60fdb795016459cde2
maitreyyi <matreyisinha@gmail.com>
2024-08-11 17:58:44 -0700

    Fix chat api issue

--- frontend/src/components/chatbox.jsx
+++ frontend/src/components/chatbox.jsx
@@ -83,7 +83,7 @@ export default function Chatbox() {
     ]);
 
     try {
-      const response = await fetch("http://localhost:5000/chat", {
+      const response = await fetch("/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
